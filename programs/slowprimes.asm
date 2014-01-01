70579ab2161080b8b7d19c17b2d4f1bd9c15e252
ejrh <ejrh00@gmail.com>
2014-01-01 18:40:56 +1300

    Write another test program, one that is compuationally intensive but doesn't use memory.

---
+++ programs/slowprimes.asm
@@ -0,0 +1,32 @@
+; SLOW PRIMES EXAMPLE
+primes::
+    mov 1, $r1                    ; Increment value
+    mov 10000, $r3                ; Maximum number to search up to
+    mov 1, $r2                    ; Number just before first candidate to check
+
+new_candidate: add $r1, $r2, $r2             
+    slt $r3, $r2, $r6  ; Check if reached maximum number
+    br $r6, stop
+    
+    mov 2, $r8                    ; Start at first divisor, 2
+check_divisor: mov 0, $r9
+    
+    ;out $r8, 3
+next_multiple: add $r8, $r9, $r9
+    ;out $r9, 4
+    slt $r2, $r9, $r6             ; Check if past candidate
+    br $r6, next_divisor          ; If so, move on to the next prime divisor
+    sub $r2, $r9, $r6             ; Check if multiple is equal to candidate
+    br $r6, next_multiple         ; If not, move on to the next multiple
+    ;out $r2, 2
+    jmp new_candidate             ; Otherwise, move on to the next candidate
+    
+next_divisor: add $r1, $r8, $r8   ; Next divisor
+    slt $r8, $r2, $r6
+    br $r6, check_divisor
+    
+    out $r2, 1                    ; Print
+    jmp new_candidate             ; No divisors found
+
+stop: out $r0, 0
+    jmp primes
