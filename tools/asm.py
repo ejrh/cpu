6ae99b6b9423681620f3957c6d3fb24e5b863df0
ejrh <ejrh00@gmail.com>
2014-01-06 19:41:05 +1300

    Write some unit tests for the assembler.

--- tools/asm.py
+++ tools/asm.py
@@ -1,5 +1,6 @@
 import sys
 import re
+from optparse import OptionParser
 
 hex_digits = '0123456789ABCDEF'
 
@@ -37,6 +38,7 @@ instr_defs = {
     'llow b,r': 'k14 r2 b1',
     'lhigh b,r': 'k15 r2 b1',
     
+    'nop': 'k0 k0 k0 k0',
     'mov r,r': 'k0 r2 k0 r1',
     'mov s,r': 'k14 r2 b1 k15 r2 k0 k0',
     'mov b,r': 'k14 r2 b1 k15 r2 k0 k0',
@@ -49,11 +51,12 @@ arg_separator_pattern = re.compile(r'(?:\s|,)+')
 line_pattern = re.compile(r'^\s* ((?P<label> [A-Za-z_][A-Za-z_0-9]*)\s*::?)? \s* ((?P<instr> [A-Za-z_][A-Za-z_0-9]*) (?P<args> ( (\s|,)+ ([$]r\d+|\d+|[A-Za-z_][A-Za-z_0-9]*) )*))?\s*$', re.VERBOSE)
 
 class Assembler(object):
-    def __init__(self):
+    def __init__(self, options):
+        self.options = options
         self.position = 0
         self.instructions = []
         self.labels = {}
-        self.has_errors = False
+        self.errors = []
     
     def read_file(self, filename):
         self.current_filename = filename
@@ -63,6 +66,13 @@ class Assembler(object):
             self.current_line += 1
             self.read_line(ln)
         f.close()
+    
+    def read_str(self, s):
+        self.current_filename = '<str>'
+        self.current_line = 0
+        for ln in s.split('\n'):
+            self.current_line += 1
+            self.read_line(ln)
     
     def read_line(self, ln):
         ln = comment_pattern.sub('', ln).strip(' \r\n\t\f')
@@ -76,7 +86,7 @@ class Assembler(object):
         label = m.group('label')
         instr = m.group('instr')
         args_str = m.group('args')
-        if args_str is not None:
+        if args_str:
             args = arg_separator_pattern.split(args_str.strip())
         else:
             args = []
@@ -104,6 +114,9 @@ class Assembler(object):
         if arg[0] == '$':
             return int(arg[2:])
         elif not arg[0].isdigit():
+            if arg not in self.labels:
+                self.error("""Unknown label '%s'""" % arg)
+                return 0
             lbl_pos = self.labels[arg]
             return lbl_pos - position
         else:
@@ -120,8 +133,9 @@ class Assembler(object):
         return l/4
 
     def error(self, msg):
-        print >>sys.stderr, '%s:%d %s' % (self.current_filename, self.current_line, msg)
-        self.has_errors = True
+        msg = '%s:%d: %s' % (self.current_filename, self.current_line, msg)
+        print >>sys.stderr, msg
+        self.errors.append(msg)
     
     def add_label(self, label):
         if label in self.labels:
@@ -147,14 +161,7 @@ class Assembler(object):
         self.instructions.append((self.position, label, instr, args, template))
         self.position += self.get_instr_len(template)
 
-    def write_program(self):
-        if self.has_errors:
-            self.error("""Program not written due to errors""")
-            return
-        for instr in self.instructions:
-            self.write_instruction(instr)
-    
-    def write_instruction(self, instr):
+    def generate_instruction(self, instr):
         pos, label, instr, args, template = instr
         ln = ''
         for p in template:
@@ -176,15 +183,42 @@ class Assembler(object):
             label_str = label + ': '
         else:
             label_str = ''
-        print "%-8s // %s%s %s" % (ln, label_str, instr, ','.join(args))
-                
+        comment = "%s%s %s" % (label_str, instr, ','.join(args))
+        self.add_line(ln, comment)
+    
+    def generate_program(self):
+        self.program_lines = []
+        for instr in self.instructions:
+            self.generate_instruction(instr)
+        if self.errors:
+            self.program_lines = []
+            self.error("""Program not written due to errors""")
+            return
+    
+    def add_line(self, line, comment=None):
+        if self.options.comments and comment is not None:
+            line = "%-8s // %s" % (line, comment)
+        self.program_lines.append(line)
+    
+    def write_str(self):
+        self.generate_program()
+        return '\n'.join(self.program_lines)
     
 def main():
     filename = sys.argv[1]
+    usage = """usage: %prog PATH [-c] INPUT"""
+    desc = """Assemble a program for the CPU"""
+    parser = OptionParser(usage=usage, description=desc)
+    parser.add_option("-c", "--comments", metavar="COMMENTS",
+                      action="store_true", dest="comments", default=False,
+                      help="enable annotation comments")
+
+    options, args = parser.parse_args()
     
-    asm = Assembler()
-    asm.read_file(filename)
-    asm.write_program()
+    asm = Assembler(options)
+    for filename in args:
+        asm.read_file(filename)
+    print asm.write_str()
 
 
 if __name__ == '__main__':
---
+++ tools/asm_tests.py
@@ -0,0 +1,69 @@
+import asm
+import unittest
+
+class Options(object):
+    def __init__(self):
+        self.comments = False
+
+class AsmTests(unittest.TestCase):
+    
+    def setUp(self):
+        options = Options()
+        self.asm = asm.Assembler(options)
+    
+    def assertSuccess(self, input, expected):
+        self.asm.read_str(input)
+        output = self.asm.write_str()
+        self.assertEquals(expected, output.strip())
+        self.assertEquals(self.asm.errors, [])
+    
+    def assertError(self, input, expected_error):
+        self.asm.read_str(input)
+        output = self.asm.write_str()
+        self.assertEquals('', output.strip())
+        self.assertIn(expected_error, '\n'.join(self.asm.errors))
+    
+    def testAdd(self):
+        input = """add $r1, $r2, $r3"""
+        expected = """0312"""
+        self.assertSuccess(input, expected)
+
+    def testNop(self):
+        input = """nop"""
+        expected = """0000"""
+        self.assertSuccess(input, expected)
+
+    def testLoadWord(self):
+        input = """mov 1234, $r1"""
+        expected = """E1D2F104"""
+        self.assertSuccess(input, expected)
+
+    def testSimpleJump(self):
+        input = """label1: jmp label1"""
+        expected = """C000"""
+        self.assertSuccess(input, expected)
+
+    def testComment(self):
+        input = """; A comment"""
+        expected = """"""
+        self.assertSuccess(input, expected)
+
+    def testUnknownLabel(self):
+        input = """label1: jmp label2"""
+        self.assertError(input, "Unknown label")
+
+    def testDuplicateLabel(self):
+        input = """label1:
+label1:"""
+        self.assertError(input, "already defined")
+
+    def testUnrecognisedInstruction(self):
+        input = """qwerty $r0"""
+        self.assertError(input, "not recognised")
+
+    def testUnparseableInstruction(self):
+        input = """!@#$%"""
+        self.assertError(input, "Cannot parse line")
+
+if __name__ == '__main__':
+    unittest.main()
