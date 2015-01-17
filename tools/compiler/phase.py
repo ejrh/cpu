80a008aa2cd9da08e77e3b5c8a3ab3030a065949
ejrh <ejrh00@gmail.com>
2015-01-18 00:40:45 +1300

    Report number of new errors and warnings in each phase.

--- tools/compiler/phase.py
+++ tools/compiler/phase.py
@@ -17,6 +17,7 @@ class Phase(object):
     
     def run(self):
         start_time = time.time()
+        start_errors, start_warnings = (self.errors.num_errors, self.errors.num_warnings)
         self.log("Running %s" % self.name)
         try:
             return self.run_phase()
@@ -26,6 +27,9 @@ class Phase(object):
         finally:
             stop_time = time.time()
             self.log("Time spent on %s: %0.3f" % (self.name, stop_time - start_time))
+            stop_errors, stop_warnings = (self.errors.num_errors, self.errors.num_warnings)
+            if start_errors != stop_errors or start_warnings != stop_warnings:
+                self.log("%d new errors and %d new warnings" % (stop_errors - start_errors, stop_warnings - start_warnings))
 
     def log(self, msg):
         print >>sys.stderr, msg
